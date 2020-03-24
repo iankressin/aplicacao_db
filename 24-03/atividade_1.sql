3c7e27d9ea6a7a014e83569c45404ce6d1f130c6
Ian <ianguimaraes31@gmail.com>
2020-03-24 19:56:52 -0300

    Atividade 1 - questoes de 1 a 3

---
+++ 24-03/atividade_1.sql
@@ -0,0 +1,74 @@
+-- Questão 1
+create table cliente (
+	idCliente serial primary key,
+	nomeCliente varchar(50) not null,
+	rg varchar(12) not null,
+	cpf varchar(14) not null,
+	telefone varchar(12)
+)
+
+create table venda (
+	idVenda serial primary key,
+	idCliente integer,
+	dataVenda date,
+	precoVenda numeric,
+	constraint fk_cliente foreign key (idCliente) references cliente(idCliente),
+	constraint min_venda check (precoVenda >= 2),
+	constraint max_venda check (precoVenda <= 10)
+)
+
+create table artista (
+	idArtista serial primary key,
+	nomeArtista varchar(50) not null,
+	telefone varchar(12),
+	observacao text
+)
+
+create table obra (
+	idObra serial primary key,
+	idArtista integer,
+	idVenda integer,
+	titulo varchar(50) not null,
+	descricao text,
+	dataAquisicao date,
+	precoCompra numeric(10,2),
+	disponivelVenda boolean,
+	constraint fk_artista foreign key (idArtista) references artista(idArtista),
+	constraint fk_venda foreign key (idVenda) references venda(idVenda)
+)
+
+create table exposicao(
+	idExposicao serial primary key,
+	dataInicio date not null,
+	dataFim date,
+	nomeExposicao varchar(45)
+)
+
+create table obra_exposicao(
+	idObra integer,
+	idExposicao integer,
+	constraint fk_obra foreign key (idObra) references obra(idObra),
+	constraint fk_exposicao foreign key (idExposicao) references exposicao(idExposicao),
+	constraint pk_obra_exposicao primary key (idObra, idExposicao)
+)
+
+-- Questão 2
+drop table obra_exposicao
+drop table exposicao
+drop table obra
+drop table artista
+drop table venda
+drop table cliente
+
+-- Questão 3
+create table endereco (
+	idEndereco serial primary key,
+	rua varchar(100),
+	numero integer,
+	bairro varchar(100),
+	estado varchar(50),
+	pais varchar(50),
+	cep varchar(10)
+)
+
+
